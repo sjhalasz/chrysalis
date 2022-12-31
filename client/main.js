bc88fe06782c4b5039de26795131ebb6f5db38f5
Steven J. Halasz <sjhalasz@gmail.com>
2022-12-31 17:18:55 -0500

    Moved set of METEOR_SETTINGS to server.

--- client/main.js
+++ client/main.js
@@ -6,14 +6,6 @@ import '../api/methods/ContactsMethods';
 import '../api/methods/TransactionsMethods';
 
 Meteor.startup(() => {
-  process.env.METEOR_SETTINGS = {
-    "packages": {
-      "quave:email-postmark": {
-        "from": "noreply@gmail.com",
-        "apiToken": "fedd0c18-5c64-46c2-91f7-6070858e7249"
-      }
-    }
-  }
   const root = createRoot(document.getElementById('react-target'));
   root.render(<App />);
 });
--- server/main.js
+++ server/main.js
@@ -8,3 +8,13 @@ import '../api/publications/WalletsPublications';
 import '../infra/CustomError';
 import '../infra/accounts';
 import '../infra/roles';
+
+process.env.METEOR_SETTINGS = 
+{
+    "packages": {
+      "quave:email-postmark": {
+        "from": "noreply@gmail.com",
+        "apiToken": "fedd0c18-5c64-46c2-91f7-6070858e7249"
+      }
+    }
+  };
