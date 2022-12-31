f433fea5d5bc8beba365a48acae53bd552417fa4
Steven J. Halasz <sjhalasz@gmail.com>
2022-12-31 16:46:53 -0500

    Set environment variable for postmark.

--- client/main.js
+++ client/main.js
@@ -6,6 +6,14 @@ import '../api/methods/ContactsMethods';
 import '../api/methods/TransactionsMethods';
 
 Meteor.startup(() => {
+  process.env.METEOR_SETTINGS = {
+    "packages": {
+      "quave:email-postmark": {
+        "from": "noreply@gmail.com",
+        "apiToken": "fedd0c18-5c64-46c2-91f7-6070858e7249"
+      }
+    }
+  }
   const root = createRoot(document.getElementById('react-target'));
   root.render(<App />);
 });
