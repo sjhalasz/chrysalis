277a289489a67487d219a6aea0ad128d9b61e34f
Steven J. Halasz <sjhalasz@gmail.com>
2023-01-17 17:58:12 -0500

    Added commenting.

--- ui/Story.js
+++ ui/Story.js
@@ -5,19 +5,34 @@ import { SuccessAlert } from './components/SuccessAlert';
 import { StoriesCollection } from '../api/collections/StoriesCollection';
 import { useSubscribe, useFind } from 'meteor/react-meteor-data';
 
+{/* This enables a logged-in user to write and edit stories.  */}
 export const Story = () => {
+    {/* initialText is what an empty story text starts with.  */}
+    {/*   We will use newline + tab when Enter is pressed, */}
+    {/*   but this will give first line indent of first paragraph. */}
     const initialText = '\t';
+    {/* These are the React variables for story data.  */}
     const [title, setTitle] = React.useState(''); 
     const [text, setText] = React.useState(initialText);
     const [published, setPublished] = React.useState();
+    {/* These are used to display messages to the user.  */}
     const [error, setError] = React.useState('');
     const [success, setSuccess] = React.useState('');
+    {/* When the user selects to start a new story, this is true.  */}
     const [newStory, setNewStory] = React.useState(true);
+    {/* The id of the currently displayed story.  */}
     const [storyId, setStoryId] = React.useState('new story');
+    {/* When a new story is saved for the first time,  */}
+    {/*   this is set to true so the story id will be read in after. */}
     const [needStoryId, setNeedStoryId] = React.useState(false);
+    {/* This is set to true when there are unsaved changes,  */}
+    {/*   and will be used to warn the user when navigating away.  */}
     const [unsavedChanges, setUnsavedChanges] = React.useState(false);
+    {/* We will be handling the Enter key by inserting tab after it.  */}
+    {/*   This is needed to preserve the position of the cursor in that case. */}
     const [cursorPosition, setCursorPosition] = React.useState(0);
 
+    {/* Function to display an error message for 5 seconds.  */}
     const showError = ({ message }) => {
       setError(message);
       setTimeout(() => {
@@ -26,15 +41,24 @@ export const Story = () => {
       return true;
     };
 
-      const showSuccess = ({ message }) => {
-        setSuccess(message);
-        setTimeout(() => {
-          setSuccess('');
-        }, 5000);
-      };
-    
+ {/* Function to display success message for 5 seconds.  */}
+    const showSuccess = ({ message }) => {
+      setSuccess(message);
+      setTimeout(() => {
+        setSuccess('');
+      }, 5000);
+    };
+
+    {/* saveStory is called when the Save Story button is clicked.  */}
     const saveStory = () => {
+      {/* Clear any extant messages.  */}
       setError('');
+      setSuccess('');
+      {/* Call the Meteor method to save the story.  */}
+      {/*   The method will update the document for existing storyId */}
+      {/*   or insert a new document if storyId does not exist. */}
+      {/*   needStoryId is set to true in case it was a new story. */}
+      {/*   unsavedChanges is set to false because changes were saved. */}
       Meteor.call(
         'story.save',
         { storyId, title, text, published },
@@ -44,20 +68,28 @@ export const Story = () => {
           showSuccess({ message:"Story saved."})+
           setNeedStoryId(true)+
           setUnsavedChanges(false);
-          ;
         }
-    );
-  }
+      );
+    }
 
-  const isLoadingStories = useSubscribe('myStories');
-  const storiesCursor = useFind(() => StoriesCollection.find({}));
-  const helpMessage = "Select a story to edit, or select '* new story' to start a new one. When you're ready to make it public, click on the 'Publish Story' checkbox. Click on the 'Save Changes' button to save."
-  
+    {/* The myStories publication returns all stories owned by  */}
+    {/*   the currently logged-in user. */}
+
+    const isLoadingStories = useSubscribe('myStories');
+    {/* Get a cursor over all stories owned by this user.  */}
+    const storiesCursor = useFind(() => StoriesCollection.find({}));
+    {/* Help message for this page.  */}
+    const helpMessage = "Select a story to edit, or select '* new story' to start a new one. When you're ready to make it public, click on the 'Publish Story' checkbox. Click on the 'Save Changes' button to save."
+
+    {/* Return HTML for creating/editing stories.  */}
     return (
       <div className="items-center">
        <h3 className=" text-lg text-base font-medium">
         Write Stories 
       </h3>
+      {/* newStory is a toggle that is set true when the user   */}
+      {/*   first selects to add a new story. A blank new story */}
+      {/*   is initialized, then the toggle is turned off. */}
       {(newStory ? 
         (setStoryId("new story")+
         setTitle("")+
@@ -65,17 +97,29 @@ export const Story = () => {
         setPublished(false)+ 
         setNewStory(false)
         ):"")}
+      {/* needStoryId is a toggle set to true when a new story is saved.  */}
+      {/*   The storyId is read from the database, then the toggle */}
+      {/*   is turned off.*/}
       {needStoryId ? 
         storiesCursor.map((story) => {
-        if(story.title == title){
-          setStoryId(story._id)+
-          setNeedStoryId(false);
-        }
-      }) : ''}
+          if(story.title == title){
+            setStoryId(story._id)+
+            setNeedStoryId(false);
+          }
+        }) : ''}
       <div className='h-20'>
-      {error ? <ErrorAlert message = {error} /> : <SuccessAlert message={success||helpMessage} />}
+      {/* Display an error message or success message or help message.  */}
+      {error ? 
+        <ErrorAlert message = {error} /> 
+        : <SuccessAlert message={success||helpMessage} 
+        />
+      }
       </div>
+      {/* The input form for selecting story and entering  */}
+      {/*   title, text, and publish switch.*/}
       <form className="mt-1">
+
+        {/* The select control. */}
           <div className="col-span-6 sm:col-span-6 lg:col-span-2"> 
             <label
               htmlFor="select"
@@ -83,6 +127,16 @@ export const Story = () => {
             >
               Select Story to Edit
             </label>
+            {/* The value is equal to the title.  */}
+            {/*   On change, if unsaved changes, ignore selection */}
+            {/*   and give and error message.*/}
+            {/*   Otherwise, clear errors and storyId and */}
+            {/*   loop through all user's stories until you find*/}
+            {/*   the one selected. It will be found unless the */}
+            {/*   user selected new story, in which case*/}
+            {/*   newStory will be true and storyId will be empty.*/}
+            {/*   For other than new story, the title, text and */}
+            {/*   published switch are set from the database. */}
             <select
               id="select"
               value={title}
@@ -105,7 +159,10 @@ export const Story = () => {
                 }}
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             >
+              {/* The first option is to create a new story. */}
+              {/*   React requires a 'key' for options and lists.*/}
               <option key = ""> * new story </option>
+              {/* Other options are taken from the title of user's stories.  */}
               {storiesCursor.map((story) => (
                 <option key = {story._id}>
                   {story.title}
@@ -113,7 +170,8 @@ export const Story = () => {
               ))}
               </select>
           </div>
-  
+
+          {/* The Title control. onInput sets unsavedChanges to true. */}
           <div className="col-span-6 sm:col-span-3 lg:col-span-2">
             <label
               htmlFor="title"
@@ -130,28 +188,39 @@ export const Story = () => {
               onChange={(e) => setTitle(e.target.value)}
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               wrap="soft"
-/>
-         </div> 
+            />
+          </div> 
 
-        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          {/* The text control.   */}
+          {/*   onKeyDown captures the Enter key and adds a tab after*/}
+          {/*   The 'foo'=== forces return of false.*/}
+          {/*   The cursorPosition captures the selectionStart*/}
+          {/*   and then is used to set selectionStart and selectionEnd.*/}
+          {/*   I don't know why this works but it does. */}
+
+          <div className="col-span-6 sm:col-span-3 lg:col-span-2">
             <label
               htmlFor="text"
               className="block text-sm font-medium text-gray-700"
             >
-              Text of Story - Use shift-enter instead of enter for new paragraph.
+              Text of Story - Enter adds a tab for paragraph indent.
             </label>
             <textarea
               id="text"
               value={text}
               onKeyDown = {(e) => {
-              return (e.key == 'Enter') ? 
-              'foo'===((setText(text.slice(0,e.target.selectionStart) +
-               '\n\t' + 
-               text.slice(e.target.selectionStart))) +
-               setCursorPosition( e.target.selectionStart + 2) +
-               e.preventDefault() +
-               (e.target.selectionEnd = e.target.selectionStart = cursorPosition))  : 
-               null
+                return (e.key == 'Enter') 
+                  ?'foo'===(
+                    (setText(
+                      text.slice(0,e.target.selectionStart) 
+                      + '\n\t' 
+                      + text.slice(e.target.selectionStart))
+                    ) 
+                    + setCursorPosition( e.target.selectionStart + 2) 
+                    + e.preventDefault() 
+                    + (e.target.selectionEnd = e.target.selectionStart = cursorPosition)
+                  )  
+                  : null
               }}
               onInput={(e) => {
                 setUnsavedChanges(true);
@@ -160,9 +229,9 @@ export const Story = () => {
               className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               wrap="soft"
               rows="8"
-/>
+            />
          </div> 
-    
+         {/* Checking this box causes story to be made public.  */}
          <div className="">
             <input
               type = "checkbox"
@@ -181,7 +250,8 @@ export const Story = () => {
             </label>
          </div> 
 
-     <div className="px-2 py-3 ">
+         {/* The Save Story button.  */}
+         <div className="px-2 py-3 ">
           <button 
             type="button"
             onClick={saveStory}
--- ui/StoryList.js
+++ ui/StoryList.js
@@ -3,20 +3,46 @@ import React, { memo } from 'react';
 import { StoriesCollection } from '../api/collections/StoriesCollection';
 import { useSubscribe, useFind } from 'meteor/react-meteor-data';
 import { Loading } from './components/Loading';
+{/* Create a list of stories from all users where the stories are  */}
+{/*   flagged as 'published'. The viewer can scroll through the stories  */}
+{/*   using 'Previous' and 'Next' buttons. Logged-on users can also   */}
+{/*   add comments. */}
  
 export const StoriesList = () => {
+  {/* The 'allStories' Meteor publication allows the retrieval of  */}
+  {/*   stories from any user where the story is flagged as 'published' */}
   const isLoading = useSubscribe('allStories');
+  {/* These React variables are used when paging through stories.  */}
+  {/*   currentKey is set to the createdAt timestamp of the currently */}
+  {/*   displayed story when the Previous or Next button is clicked.*/}
+  {/*   It starts as the current timestamp so that the first story */}
+  {/*   will be the newest story that is before the current moment.*/}
   const [currentKey, setCurrentKey] = React.useState(new Date());
+  {/*   The direction variable is -1 for 'Next' and 1 for 'Previous'*/}
   const [direction, setDirection] = React.useState(-1);
  
+  {/* nextStory is called when the 'Next' button is clicked.  */}
+  {/*   It sets direction to -1 to indicate reverse sort, and*/}
+  {/*   sets currentKey to be the createdAt timestamp of the*/}
+  {/*   currently-displayed story. Because these variables are*/}
+  {/*   handled by React, this causes the story retrieval routine */}
+  {/*   to be re-run.*/}
+
   const nextStory = () => {
     setDirection(-1);
     setCurrentKey(story.createdAt);
   }
+
+  {/*Likewise for the 'Previous' button.   */}
+
   const previousStory = () => {
     setDirection(1);
     setCurrentKey(story.createdAt);
   }
+
+  {/* StoryItem formats the story. It displays the user name and the title. */}
+  {/*   It then displays the story text underneath. In future it will also*/}
+  {/*   display the comments and allow posting comments.*/}
 
     const StoryItem = ({ story }) => (
     <>
@@ -30,66 +56,103 @@ export const StoriesList = () => {
           </p>
         </div>
         </div>
+          {/* The story text is displayed in a <pre> so that the  */}
+          {/*   tabs and newlines are shown as entered by the author,*/}
           <pre 
           className="text-sm font-medium text-gray-500">
-            {'\t' + story.text.replace(/(?![^\n]{1,64}$)([^\n]{1,64})\s/g, '$1\n')}
+          {/* The replace here does a word wrap within 64 characters.  */}
+          {/*   With <pre> there is otherwise no word wrap. */}
+              {'\t' + story.text.replace(/(?![^\n]{1,64}$)([^\n]{1,64})\s/g, '$1\n')}
           </pre>
     </>
   );
 
+  {/* When the page is loading. give an approrpiate message.  */}
   if (isLoading()) {
     return <Loading />;
   }
   else
 {
+  {/* Otherwise, load the newest story.  */}
+  {/* When the page first loads, or when Next is clicked, direction is set to -1.  */}
   if(direction == -1){
-  next = StoriesCollection.find(
-    {createdAt: {$lt: currentKey}}
-    ,{sort:{createdAt: -1}, limit: 2}
-  );
-  nextExists = ( next.count() > 1);
-  story = next.fetch()[0];
-  var  previous = StoriesCollection.find(
-      {createdAt: {$gt: story.createdAt}}
-      ,{sort:{createdAt: 1}, limit: 2}
+    {/* Get a cursor pointing to the next two stories.  */}
+    {/* currentKey is initially the current moment, or when Next is clicked,  */}
+    {/*   the createdAt timestamp of the currently-displayed story. */}
+    {/*   With reverse sorting, from latest to earliest, we want the next */}
+    {/*   two stories that are earlier than the current story.  */}
+    next = StoriesCollection.find(
+      {createdAt: {$lt: currentKey}}
+      ,{sort:{createdAt: -1}, limit: 2}
     );
-  previousExists = (previous.count() > 0) ;
+    {/* If there are two, there is another earlier one   */}
+    {/*   so the Next button will be enabled. */}  
+    nextExists = ( next.count() > 1);
+    {/* Retrieve the next earlier story from the cursor.  */}
+    story = next.fetch()[0];
+    {/* Check if there's a previous (older) story. */}
+    {/*   This is necessary to disable the Previous button when the*/}
+    {/*   page is first loaded.*/}
+    {/*   With ascending sort, earliest to latest story,*/}
+    {/*   find the next story that is later than the story*/}
+    {/*   we just retrieved.*/}
+    var  previous = StoriesCollection.find(
+      {createdAt: {$gt: story.createdAt}}
+      ,{sort:{createdAt: 1}, limit: 1}
+    );
+    {/* We will enable the Previous button if one exists.  */}
+    previousExists = (previous.count() > 0) ;
   }
   else {
+    {/* Otherwise, the Previous button was clicked.  */}
+    {/*   Get a cursor with ascending sort, earliest to latest,*/}
+    {/*   with createdAt greater than the story that was displayed */}
+    {/*   when Previous was clicked. */}
     previous = StoriesCollection.find(
       {createdAt: {$gt: currentKey}}
       ,{sort:{createdAt: 1}, limit: 2}
     );
+    {/* We will enable the Previous button if there are at least 2,  */}
+    {/*   the one we are about to display and another one. */}
     previousExists = (previous.count() > 1) ;
+    {/* There is always a next if we got here by clicking Previous.  */}
     nextExists = true;
+    {/* Get the story to display  */}
     story = previous.fetch()[0];
   }
   }
-
+  {/* This is the HTML to return to the page.  */}
   return (
     <div>
            <div className="px-2 py-3 ">
-          <button 
-            type="button"
-            onClick={previousStory}
-            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 active:bg-orange-900"
-            disabled={!previousExists}
->
-            Previous Story
-          </button>
-
-          <button 
-            type="button"
-            onClick={nextStory}
-            className="ml-2 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 active:bg-orange-900"
-            autoFocus
-            disabled={!nextExists}
-          >
-            Next Story
-          </button>
-        </div> 
-        <StoryItem story = {story} 
+             {/* The Previous button.  */}
+             {/* Note that React has a hack that treats   */}
+             {/*   disabled={true/false} by including 'disabled' */}
+             {/*   if true and omitting it if false. */}
+             <button 
+               type="button"
+               onClick={previousStory}
+               className="disabled:opacity-50 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 active:bg-orange-900"
+               disabled={!previousExists}
+             >
+               Previous Story
+             </button>
+             
+             {/* The Next button.  */}
+             <button 
+               type="button"
+               onClick={nextStory}
+               className="disabled:opacity-50 ml-2 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 active:bg-orange-900"
+               autoFocus
+               disabled={!nextExists}
+             >
+               Next Story
+             </button>
+           </div> 
+           {/* Display the story. It is intentionally not handled  */}
+           {/*   by React because we don't want the story to change */}
+           {/*   or disappear while someone is reading it. */}
+           <StoryItem story = {story} 
          />
-      
     </div>);
 }
